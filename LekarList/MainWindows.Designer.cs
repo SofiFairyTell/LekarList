12e8bff6b2e3cd206000f29ff5f66bae25a22201
SofiFairyTell <fairytellsblog@gmail.com>
2020-07-14 11:24:10 +0300

    Добавлен DGV

--- LekarList/MainWindows.Designer.cs
+++ LekarList/MainWindows.Designer.cs
@@ -32,27 +32,42 @@
             this.AddButton = new System.Windows.Forms.Button();
             this.LBdata = new System.Windows.Forms.ListBox();
             this.SortButton = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.настройкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.добавитьДанныеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.GroupNameColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DescriptColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // treeView1
             // 
-            this.treeView1.Dock = System.Windows.Forms.DockStyle.Left;
             this.treeView1.ImeMode = System.Windows.Forms.ImeMode.On;
             this.treeView1.Location = new System.Drawing.Point(0, 0);
             this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(304, 450);
+            this.treeView1.Size = new System.Drawing.Size(304, 390);
             this.treeView1.TabIndex = 0;
             this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
             this.treeView1.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseDoubleClick);
             // 
             // AddButton
             // 
-            this.AddButton.Location = new System.Drawing.Point(585, 217);
+            this.AddButton.Location = new System.Drawing.Point(895, 71);
             this.AddButton.Name = "AddButton";
-            this.AddButton.Size = new System.Drawing.Size(117, 34);
+            this.AddButton.Size = new System.Drawing.Size(102, 34);
             this.AddButton.TabIndex = 2;
             this.AddButton.Text = "Добавить";
             this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Visible = false;
             this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
             // 
             // LBdata
@@ -60,44 +75,159 @@
             this.LBdata.FormattingEnabled = true;
             this.LBdata.ItemHeight = 16;
             this.LBdata.Items.AddRange(new object[] {
-            "A,0",
-            "B,0",
-            "D,0",
-            "D01,1",
-            "D01A,2",
-            "C,0",
-            "D01AA,3",
-            "C01,1",
-            "1C01"});
-            this.LBdata.Location = new System.Drawing.Point(365, 24);
+            "A",
+            "B",
+            "D",
+            "D01",
+            "D01",
+            "D01AA"});
+            this.LBdata.Location = new System.Drawing.Point(856, 111);
             this.LBdata.Name = "LBdata";
-            this.LBdata.Size = new System.Drawing.Size(337, 180);
+            this.LBdata.Size = new System.Drawing.Size(153, 116);
             this.LBdata.TabIndex = 3;
+            this.LBdata.Visible = false;
             this.LBdata.SelectedIndexChanged += new System.EventHandler(this.LBdata_SelectedIndexChanged);
             // 
             // SortButton
             // 
-            this.SortButton.Location = new System.Drawing.Point(442, 217);
+            this.SortButton.Location = new System.Drawing.Point(895, 31);
             this.SortButton.Name = "SortButton";
             this.SortButton.Size = new System.Drawing.Size(102, 34);
             this.SortButton.TabIndex = 4;
             this.SortButton.Text = "Сортировка";
             this.SortButton.UseVisualStyleBackColor = true;
+            this.SortButton.Visible = false;
             this.SortButton.Click += new System.EventHandler(this.SortButton_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.настройкиToolStripMenuItem,
+            this.выходToolStripMenuItem,
+            this.выходToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1009, 28);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // настройкиToolStripMenuItem
+            // 
+            this.настройкиToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.добавитьДанныеToolStripMenuItem});
+            this.настройкиToolStripMenuItem.Name = "настройкиToolStripMenuItem";
+            this.настройкиToolStripMenuItem.Size = new System.Drawing.Size(96, 24);
+            this.настройкиToolStripMenuItem.Text = "Настройки";
+            // 
+            // добавитьДанныеToolStripMenuItem
+            // 
+            this.добавитьДанныеToolStripMenuItem.Name = "добавитьДанныеToolStripMenuItem";
+            this.добавитьДанныеToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.добавитьДанныеToolStripMenuItem.Text = "Добавить данные";
+            this.добавитьДанныеToolStripMenuItem.Click += new System.EventHandler(this.добавитьДанныеToolStripMenuItem_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.tabControl1.Location = new System.Drawing.Point(0, 28);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(768, 422);
+            this.tabControl1.TabIndex = 6;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.Silver;
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Controls.Add(this.treeView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(760, 393);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "АТХ классификация";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.Silver;
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(738, 393);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Заболевания";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.выходToolStripMenuItem.Text = "Помощь";
+            // 
+            // выходToolStripMenuItem1
+            // 
+            this.выходToolStripMenuItem1.Name = "выходToolStripMenuItem1";
+            this.выходToolStripMenuItem1.Size = new System.Drawing.Size(65, 24);
+            this.выходToolStripMenuItem1.Text = "Выход";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.GroupNameColumn,
+            this.DescriptColumn});
+            this.dataGridView1.Location = new System.Drawing.Point(311, 7);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(446, 150);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // GroupNameColumn
+            // 
+            this.GroupNameColumn.Frozen = true;
+            this.GroupNameColumn.HeaderText = "";
+            this.GroupNameColumn.MaxInputLength = 30;
+            this.GroupNameColumn.Name = "GroupNameColumn";
+            this.GroupNameColumn.ReadOnly = true;
+            this.GroupNameColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // DescriptColumn
+            // 
+            this.DescriptColumn.Frozen = true;
+            this.DescriptColumn.HeaderText = "";
+            this.DescriptColumn.Name = "DescriptColumn";
+            this.DescriptColumn.ReadOnly = true;
+            this.DescriptColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.DescriptColumn.Width = 300;
             // 
             // MainWindows
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.SortButton);
+            this.ClientSize = new System.Drawing.Size(1009, 450);
             this.Controls.Add(this.LBdata);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.SortButton);
             this.Controls.Add(this.AddButton);
-            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainWindows";
-            this.Text = " ";
+            this.Text = " Справочник лекарственных средств";
             this.Load += new System.EventHandler(this.MainWindows_Load_1);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -107,6 +237,17 @@
         private System.Windows.Forms.Button AddButton;
         private System.Windows.Forms.ListBox LBdata;
         private System.Windows.Forms.Button SortButton;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem настройкиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem добавитьДанныеToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GroupNameColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DescriptColumn;
     }
 }
 
